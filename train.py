c618e62ed77a435c4420265adde12cd6ab3fd435
tianyue <970157243@qq.com>
2022-08-05 15:02:42 +0800

    add code

--- train.py
+++ train.py
@@ -1,175 +1,45 @@
 import json
+import os
 import shutil
 from time import time
+
 import config
+import numpy as np
+import cv2
+import copy
 import torch
 import torch.nn.functional as F
 import torchvision
-from model.preact_resnet import  PreActResNet18
 from model.resnet import ResNet18
+from model.preact_resnet import PreActResNet18
 from model.MNISTnet import MNISTnet
 from network.models import Denormalizer
 from torch import nn
-import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from utils.dataloader import PostTensorTransform, get_dataloader
 from utils.utils import progress_bar
-import cv2
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"
+
 
 def get_model(opt):
     net = None
     optimizer = None
     scheduler = None
+
     if opt.dataset == "cifar10" or opt.dataset == "gtsrb":
         net = PreActResNet18(num_classes=opt.num_classes).to(opt.device)
     if opt.dataset == "celeba":
         net = ResNet18().to(opt.device)
     if opt.dataset == "mnist":
         net = MNISTnet().to(opt.device)
+
+    # Optimizer
     optimizer = torch.optim.SGD(net.parameters(), opt.lr, momentum=0.9, weight_decay=5e-4)
+
+    # Scheduler
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, opt.scheduler_milestones, opt.scheduler_lambda)
+
     return net, optimizer, scheduler
 
-def train(net, optimizer, scheduler, train_dl, identity_grid, noise_grid, tf_writer, epoch, opt):
-    net = nn.DataParallel(net)
-    net.to(opt.device)
-    print(" Train:")
-    net.train()
-    rate_bd = opt.ar
-    total_loss = 0
-    total_sample = 0
-    total_clean = 0
-    total_bd = 0
-    total_clean_correct = 0
-    total_bd_correct = 0
-    criterion_CE = torch.nn.CrossEntropyLoss()
-    denormalizer = Denormalizer(opt)
-    transforms = PostTensorTransform(opt).to(opt.device)
-    total_time = 0
-
-    for batch_idx, (inputs, targets) in enumerate(train_dl):
-        optimizer.zero_grad()
-        inputs, targets = inputs.to(opt.device), targets.to(opt.device)
-        bs = inputs.shape[0]
-        # Create backdoor data
-        num_bd = int(bs * rate_bd)
-        grid_temps = (identity_grid + opt.s * noise_grid / (opt.input_height // opt.ratio)) * opt.grid_rescale
-        grid_temps = torch.clamp(grid_temps, -1, 1).float()
-        for id_img in range(num_bd):
-            bbx1, bby1, bbx2, bby2 = saliency_bbox(inputs[id_img])
-            inputs[id_img:(id_img+1), :, bbx1:bbx2, bby1:bby2] = F.grid_sample(inputs[id_img:(id_img+1), :, bbx1:bbx2, bby1:bby2], grid_temps.repeat(1, 1, 1, 1), align_corners=True)
-        inputs_bd = inputs[:num_bd]
-        if opt.attack_mode == "all2one":
-            targets_bd = torch.ones_like(targets[:num_bd]) * opt.target_label
-        if opt.attack_mode == "all2all":
-            targets_bd = torch.remainder(targets[:num_bd] + 1, opt.num_classes)
-        total_inputs = torch.cat([inputs_bd, inputs[num_bd :]], dim=0)
-        total_inputs = transforms(total_inputs)
-        total_targets = torch.cat([targets_bd, targets[num_bd:]], dim=0)
-        start = time()
-        total_preds = net(total_inputs)
-        total_time += time()-start
-        loss = criterion_CE(total_preds, total_targets)
-        loss.backward()
-        optimizer.step()
-        total_sample += bs
-        total_loss += loss.detach()
-        total_clean += bs - num_bd
-        total_bd += num_bd
-        total_clean_correct += torch.sum(
-            torch.argmax(total_preds[num_bd:], dim=1) == total_targets[num_bd:]
-        )
-        total_bd_correct += torch.sum(torch.argmax(total_preds[:num_bd], dim=1) == targets_bd)
-        avg_acc_clean = total_clean_correct * 100.0 / total_clean
-        avg_acc_bd = total_bd_correct * 100.0 / total_bd
-        avg_loss = total_loss / total_sample
-        progress_bar(batch_idx, len(train_dl), "CE Loss: {:.4f} | Clean Acc: {:.4f} | Bd Acc: {:.4f} ".format(avg_loss, avg_acc_clean, avg_acc_bd))
-        # save backdoor image
-        if not batch_idx % 50:
-            if not os.path.exists(opt.temps):
-                os.makedirs(opt.temps)
-            path = os.path.join(opt.temps, "backdoor_image.png")
-            torchvision.utils.save_image(inputs_bd, path, normalize=True)
-        # tensorboard image
-        if batch_idx == len(train_dl) - 2:
-            residual = inputs_bd - inputs[:num_bd]
-            batch_img = torch.cat([inputs[:num_bd], inputs_bd, total_inputs[:num_bd], residual], dim=2)
-            batch_img = denormalizer(batch_img)
-            batch_img = F.upsample(batch_img, scale_factor=(4, 4))
-            grid = torchvision.utils.make_grid(batch_img, normalize=True)
-
-    # tensorboard information
-    if not epoch % 1:
-        tf_writer.add_scalars("Train Accuracy", {"clean": avg_acc_clean, "bd": avg_acc_bd}, epoch)
-        tf_writer.add_image("Images", grid, global_step=epoch)
-    scheduler.step()
-
-
-def eval(net, optimizer, scheduler, test_dl, identity_grid, noise_grid, best_clean_acc, best_bd_acc, tf_writer, epoch, opt):
-    net = nn.DataParallel(net)
-    net.to(opt.device)
-    print(" Eval:")
-    net.eval()
-    total_sample = 0
-    total_clean_correct = 0
-    total_bd_correct = 0
-    total_ae_loss = 0
-    criterion_BCE = torch.nn.BCELoss()
-
-    for batch_idx, (inputs, targets) in enumerate(test_dl):
-        with torch.no_grad():
-            inputs, targets = inputs.to(opt.device), targets.to(opt.device)
-            bs = inputs.shape[0]
-            total_sample += bs
-            # Clean Evaluation
-            preds_clean = net(inputs)
-            total_clean_correct += torch.sum(torch.argmax(preds_clean, 1) == targets)
-            # Backdoor Evaluation
-            grid_temps = (identity_grid + opt.s * noise_grid / (opt.input_height // opt.ratio)) * opt.grid_rescale
-            grid_temps = torch.clamp(grid_temps, -1, 1).float()
-            for idv_img in range(bs):
-                bbx1, bby1, bbx2, bby2 = saliency_bbox(inputs[idv_img])
-                inputs[idv_img:(idv_img + 1), :, bbx1:bbx2, bby1:bby2] = F.grid_sample(inputs[idv_img:(idv_img + 1), :, bbx1:bbx2, bby1:bby2], grid_temps.repeat(1, 1, 1, 1),align_corners=True)
-            inputs_bd = inputs
-            if opt.attack_mode == "all2one":
-                targets_bd = torch.ones_like(targets) * opt.target_label
-            if opt.attack_mode == "all2all":
-                targets_bd = torch.remainder(targets + 1, opt.num_classes)
-            preds_bd = net(inputs_bd)
-            total_bd_correct += torch.sum(torch.argmax(preds_bd, 1) == targets_bd)
-            acc_clean = total_clean_correct * 100.0 / total_sample
-            acc_bd = total_bd_correct * 100.0 / total_sample
-            progress_bar(batch_idx, len(test_dl), "Clean Acc: {:.4f} - Best: {:.4f} | Bd Acc: {:.4f} - Best: {:.4f}".format(acc_clean, best_clean_acc, acc_bd, best_bd_acc))
-
-    # tensorboard information
-    if not epoch % 1:
-        tf_writer.add_scalars("Test Accuracy", {"clean": acc_clean, "bd": acc_bd}, epoch)
-
-    # checkpoint
-    if acc_clean > best_clean_acc or (acc_clean > best_clean_acc - 0.1 and acc_bd > best_bd_acc):
-        print(" Saving...")
-        best_clean_acc = acc_clean
-        best_bd_acc = acc_bd
-        state_dict = {
-            "net": net.state_dict(),
-            "scheduler": scheduler.state_dict(),
-            "optimizer": optimizer.state_dict(),
-            "best_clean_acc": best_clean_acc,
-            "best_bd_acc": best_bd_acc,
-            "epoch_current": epoch,
-            "identity_grid": identity_grid,
-            "noise_grid": noise_grid,
-        }
-        torch.save(state_dict, opt.ckpt_path)
-        with open(os.path.join(opt.ckpt_folder, "results.txt"), "w+") as f:
-            results_dict = {
-                "clean_acc": best_clean_acc.item(),
-                "bd_acc": best_bd_acc.item(),
-            }
-            json.dump(results_dict, f, indent=2)
-    return best_clean_acc, best_bd_acc
 
 def saliency_bbox(img):
     opt = config.get_arguments().parse_args()
@@ -177,9 +47,8 @@ def saliency_bbox(img):
     W = size[1]
     H = size[2]
     ratio = opt.ratio
-    cut_w = int (W // ratio)
-    cut_h = int (H // ratio)
-
+    cut_w = int(W // ratio)
+    cut_h = int(H // ratio)
     # compute the image saliency map
     temp_img = img.cpu().numpy().transpose(1, 2, 0)
     saliency = cv2.saliency.StaticSaliencyFineGrained_create()
@@ -205,6 +74,148 @@ def saliency_bbox(img):
         bby1 = H - (H // opt.ratio)
         bby2 = H
     return bbx1, bby1, bbx2, bby2
+
+
+def train(net, optimizer, scheduler, train_dl, noise_grid, identity_grid, tf_writer, epoch, opt):
+    print(" Train:")
+    net.to(opt.device)
+    net.train()
+    rate_bd = opt.pc
+    total_loss_ce = 0
+    total_sample = 0
+    total_clean = 0
+    total_bd = 0
+    total_clean_correct = 0
+    total_bd_correct = 0
+    criterion_CE = torch.nn.CrossEntropyLoss()
+    denormalizer = Denormalizer(opt)
+    transforms = PostTensorTransform(opt).to(opt.device)
+    total_time = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_dl):
+        optimizer.zero_grad()
+        inputs, targets = inputs.to(opt.device), targets.to(opt.device)
+        bs = inputs.shape[0]
+        # Create backdoor data
+        num_bd = int(bs * rate_bd)
+        input_origin = copy.deepcopy(inputs[:num_bd])
+        num_cross = int(num_bd * opt.cross_ratio)
+        grid_temps = (identity_grid + opt.s * noise_grid / (opt.input_height // opt.ratio)) * opt.grid_rescale
+        grid_temps = torch.clamp(grid_temps, -1, 1).float()
+        inputs_bd = inputs[:num_bd]
+
+        for id_img in range(num_bd):
+            bbx1, bby1, bbx2, bby2 = saliency_bbox(inputs_bd[id_img])
+            temp = inputs_bd[id_img:(id_img + 1), :, bbx1:bbx2, bby1:bby2]
+            inputs_bd[id_img:(id_img + 1), :, bbx1:bbx2, bby1:bby2] = F.grid_sample(temp, grid_temps.repeat(1, 1, 1, 1),
+                                                                                    align_corners=True)
+        if opt.attack_mode == "all2one":
+            targets_bd = torch.ones_like(targets[:num_bd]) * opt.target_label
+        if opt.attack_mode == "all2all":
+            targets_bd = torch.remainder(targets[:num_bd] + 1, opt.num_classes)
+
+
+        total_inputs = torch.cat([inputs_bd, inputs[num_bd:]], dim=0)
+        total_inputs = transforms(total_inputs)
+        total_targets = torch.cat([targets_bd, targets[num_bd:]], dim=0)
+        start = time()
+        total_preds = net(total_inputs)
+        total_time += time() - start
+        loss_ce = criterion_CE(total_preds, total_targets)
+        loss = loss_ce
+        loss.backward()
+        optimizer.step()
+
+        total_sample += bs
+        total_loss_ce += loss_ce.detach()
+        total_clean += bs - num_bd - num_cross
+        total_bd += num_bd
+        total_clean_correct += torch.sum(torch.argmax(total_preds[(num_bd + num_cross):], dim=1) == total_targets[(num_bd + num_cross):])
+        total_bd_correct += torch.sum(torch.argmax(total_preds[:num_bd], dim=1) == targets_bd)
+        avg_acc_clean = total_clean_correct * 100.0 / total_clean
+        avg_acc_bd = total_bd_correct * 100.0 / total_bd
+
+        avg_loss_ce = total_loss_ce / total_sample
+        progress_bar(batch_idx, len(train_dl),"CE Loss: {:.4f} | Clean Acc: {:.4f} | Bd Acc: {:.4f} ".format(avg_loss_ce, avg_acc_clean, avg_acc_bd))
+
+        # Image for tensorboard
+        if batch_idx == len(train_dl) - 2:
+            residual = inputs_bd - input_origin
+            batch_img = torch.cat([input_origin, inputs_bd, total_inputs[:num_bd], residual], dim=2)
+            batch_img = denormalizer(batch_img)
+            batch_img = F.upsample(batch_img, scale_factor=(4, 4))
+            grid = torchvision.utils.make_grid(batch_img, normalize=True)
+
+    # for tensorboard
+    if not epoch % 1:
+        tf_writer.add_scalars("Clean Accuracy", {"Clean": avg_acc_clean, "Bd": avg_acc_bd}, epoch)
+        tf_writer.add_image("Images", grid, global_step=epoch)
+    scheduler.step()
+
+
+def eval(net, optimizer, scheduler, test_dl, noise_grid, identity_grid, best_clean_acc, best_bd_acc, tf_writer, epoch, opt):
+    print(" Eval:")
+    net.to(opt.device)
+    net.eval()
+    total_sample = 0
+    total_clean_correct = 0
+    total_bd_correct = 0
+    total_cross_correct = 0
+    for batch_idx, (inputs, targets) in enumerate(test_dl):
+        with torch.no_grad():
+            inputs, targets = inputs.to(opt.device), targets.to(opt.device)
+            bs = inputs.shape[0]
+            total_sample += bs
+
+            # Evaluate Clean
+            preds_clean = net(inputs)
+            total_clean_correct += torch.sum(torch.argmax(preds_clean, 1) == targets)
+            grid_temps = (identity_grid + opt.s * noise_grid / (opt.input_height // opt.ratio)) * opt.grid_rescale
+            grid_temps = torch.clamp(grid_temps, -1, 1).float()
+            inputs_bd = inputs
+            for idv_img in range(bs):
+                bbx1, bby1, bbx2, bby2 = saliency_bbox(inputs_bd[idv_img])
+                inputs_bd[idv_img:(idv_img + 1), :, bbx1:bbx2, bby1:bby2] = F.grid_sample(
+                    inputs_bd[idv_img:(idv_img + 1), :, bbx1:bbx2, bby1:bby2], grid_temps.repeat(1, 1, 1, 1),
+                    align_corners=True)
+            if opt.attack_mode == "all2one":
+                targets_bd = torch.ones_like(targets) * opt.target_label
+            if opt.attack_mode == "all2all":
+                targets_bd = torch.remainder(targets + 1, opt.num_classes)
+            preds_bd = net(inputs_bd)
+            total_bd_correct += torch.sum(torch.argmax(preds_bd, 1) == targets_bd)
+            acc_clean = total_clean_correct * 100.0 / total_sample
+            acc_bd = total_bd_correct * 100.0 / total_sample
+            # Evaluate cross
+            info_string = "Clean Acc: {:.4f} - Best: {:.4f} | Bd Acc: {:.4f} - Best: {:.4f}".format(acc_clean, best_clean_acc, acc_bd, best_bd_acc)
+            progress_bar(batch_idx, len(test_dl), info_string)
+    # tensorboard
+    if not epoch % 1:
+        tf_writer.add_scalars("Test Accuracy", {"Clean": acc_clean, "Bd": acc_bd}, epoch)
+
+    # Save checkpoint
+    if acc_clean > best_clean_acc or (acc_clean > best_clean_acc - 0.1 and acc_bd > best_bd_acc):
+        print(" Saving...")
+        best_clean_acc = acc_clean
+        best_bd_acc = acc_bd
+        state_dict = {
+            "netC": net.state_dict(),
+            "schedulerC": scheduler.state_dict(),
+            "optimizerC": optimizer.state_dict(),
+            "best_clean_acc": best_clean_acc,
+            "best_bd_acc": best_bd_acc,
+            "epoch_current": epoch,
+            "identity_grid": identity_grid,
+            "noise_grid": noise_grid,
+        }
+        torch.save(state_dict, opt.ckpt_path)
+        with open(os.path.join(opt.ckpt_folder, "results.txt"), "w+") as f:
+            results_dict = {
+                "clean_acc": best_clean_acc.item(),
+                "bd_acc": best_bd_acc.item(),
+            }
+            json.dump(results_dict, f, indent=2)
+    return best_clean_acc, best_bd_acc
 
 
 def main():
@@ -237,26 +248,44 @@ def main():
     else:
         raise Exception("Invalid Dataset")
 
-    # dataset and model
+    # Dataset
     train_dl = get_dataloader(opt, True)
     test_dl = get_dataloader(opt, False)
+    # prepare model
     net, optimizer, scheduler = get_model(opt)
-
-    # pretrained model
+    # Load pretrained model
     mode = opt.attack_mode
-    opt.ckpt_folder = os.path.join(opt.checkpoints, opt.dataset)
+    # opt.ckpt_folder = os.path.join(opt.checkpoints, opt.dataset)
+    opt.ckpt_folder = os.path.join(opt.checkpoints, 'cifar10-2')
     opt.ckpt_path = os.path.join(opt.ckpt_folder, "{}_{}.pth.tar".format(opt.dataset, mode))
     opt.log_dir = os.path.join(opt.ckpt_folder, "log_dir")
     if not os.path.exists(opt.log_dir):
         os.makedirs(opt.log_dir)
 
-    if opt.train_scratch:
-        print("Train from scratch")
+    if opt.continue_training:
+        if os.path.exists(opt.ckpt_path):
+            print("Continue training!!")
+            state_dict = torch.load(opt.ckpt_path)
+            net.load_state_dict(state_dict["netC"])
+            optimizer.load_state_dict(state_dict["optimizerC"])
+            scheduler.load_state_dict(state_dict["schedulerC"])
+            best_clean_acc = state_dict["best_clean_acc"]
+            best_bd_acc = state_dict["best_bd_acc"]
+            epoch_current = state_dict["epoch_current"]
+            identity_grid = state_dict["identity_grid"]
+            noise_grid = state_dict["noise_grid"]
+            tf_writer = SummaryWriter(log_dir=opt.log_dir)
+        else:
+            print("Pretrained model doesnt exist")
+            exit()
+    else:
+        print("Train from scratch!!!")
         best_clean_acc = 0.0
         best_bd_acc = 0.0
+        best_cross_acc = 0.0
         epoch_current = 0
 
-        # grid design
+        # Prepare grid
         ins = np.random.beta(1, 1, (1, 2, opt.k, opt.k)) * 2 - 1
         ins = torch.tensor(ins)
         ins = ins / torch.mean(torch.abs(ins))
@@ -268,30 +297,18 @@ def main():
         array1d = torch.linspace(-1, 1, steps=opt.input_height // opt.ratio)
         x, y = torch.meshgrid(array1d, array1d)
         identity_grid = torch.stack((y, x), dim=2)[None, ...].to(opt.device)
+
         shutil.rmtree(opt.ckpt_folder, ignore_errors=True)
         os.makedirs(opt.log_dir)
         with open(os.path.join(opt.ckpt_folder, "opt.json"), "w+") as f:
             json.dump(opt.__dict__, f, indent=2)
         tf_writer = SummaryWriter(log_dir=opt.log_dir)
-    else:
-        print("Train from pretrained model")
-        state_dict = torch.load(opt.ckpt_path)
-        net.load_state_dict(state_dict["net"])
-        optimizer.load_state_dict(state_dict["optimizer"])
-        scheduler.load_state_dict(state_dict["scheduler"])
-        best_clean_acc = state_dict["best_clean_acc"]
-        best_bd_acc = state_dict["best_bd_acc"]
-        epoch_current = state_dict["epoch_current"]
-        identity_grid = state_dict["identity_grid"]
-        noise_grid = state_dict["noise_grid"]
-        tf_writer = SummaryWriter(log_dir=opt.log_dir)
 
-    for epoch in range(epoch_current, opt.epoches):
+    for epoch in range(epoch_current, opt.n_iters):
         print("Epoch {}:".format(epoch + 1))
         train(net, optimizer, scheduler, train_dl, noise_grid, identity_grid, tf_writer, epoch, opt)
         best_clean_acc, best_bd_acc = eval(net, optimizer, scheduler, test_dl, noise_grid, identity_grid, best_clean_acc, best_bd_acc, tf_writer, epoch, opt)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
